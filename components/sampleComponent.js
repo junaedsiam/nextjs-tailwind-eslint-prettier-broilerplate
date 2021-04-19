4630bb67b297687ac4feb9e2b22e7b7904280634
junaedsiam <junaedsiam@gmail.com>
2021-04-19 15:13:38 +0600

    initial commit

---
+++ components/sampleComponent.js
@@ -0,0 +1,20 @@
+export default function SampleComponent() {
+    return (
+        <div className="container mx-auto py-8">
+            <div className="max-w-xl border-2 border-gray-200 border-opacity-50 mx-auto p-8 rounded-lg">
+                <h1 className="text-4xl font-bold mb-3">Next Js Tailwind Broilerplate</h1>
+                <p className="leading-relaxed text-gray-600 mb-6">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum impedit sint iste
+                    animi quasi, earum laboriosam, obcaecati vero reiciendis maiores dicta
+                    dignissimos autem reprehenderit. Perferendis, minus quibusdam? Vero, porro
+                    consequuntur?
+                </p>
+                <a
+                    href="#!"
+                    className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 border border-transparent rounded">
+                    Sample Button
+                </a>
+            </div>
+        </div>
+    );
+}
